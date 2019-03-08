794a287dbf997776db69cd63f276b0bd18bded93
Adam Benson <AdamBenson.vfx@gmail.com>
2019-03-08 10:29:10 -0800

    Local work.  About to pull from latest git

--- development/test_dev_2.py
+++ development/test_dev_2.py
@@ -1,119 +1,51 @@
-import os
-from glob import glob
-import re
-import win32file
-import win32con
-import win32serviceutil
 import win32service
+import win32serviceutil
 import win32event
-import servicemanager
-import socket
-import platform
-import time
-import logging
-import logging.handlers
-import shutil
-import yaml
-import psd_tools as psd
-import shotgun_api3
-from Queue import Queue
-import threading
 
-q = Queue()
-
-
-# Windows Triggers
-ACTIONS = {
-    1: "Created",
-    2: "Deleted",
-    3: "Updated",
-    4: "Renamed from something",
-    5: "Renamed to something"
-}
-# Thanks to Claudio Grondi for the correct set of numbers
-FILE_LIST_DIRECTORY = 0x0001
-
-# Dropbox Folder
-path_to_watch = "C:/Users/events/Dropbox/ASC_REMOTE"
-hDir = win32file.CreateFile(
-    path_to_watch,
-    FILE_LIST_DIRECTORY,
-    win32con.FILE_SHARE_READ | win32con.FILE_SHARE_WRITE | win32con.FILE_SHARE_DELETE,
-    None,
-    win32con.OPEN_EXISTING,
-    win32con.FILE_FLAG_BACKUP_SEMANTICS,
-    None
-)
-
-
-def process_file(filename):
-    while True:
-        thing = q.get()
-        print thing
-        q.task_done()
-
-
-T = threading.Thread(name='shit', target=process_file)
-T.setDaemon(True)
-T.start()
-
-
-class remoteAutoPublisher(win32serviceutil.ServiceFramework):
-    _svc_name_ = "RemoteAutoPublisher"
-    _svc_display_name_ = "Remote Auto Publisher"
+class PySvc(win32serviceutil.ServiceFramework):
+    # you can NET START/STOP the service by the following name
+    _svc_name_ = "PySvc"
+    # this text shows up as the service name in the Service
+    # Control Manager (SCM)
+    _svc_display_name_ = "Python Test Service"
+    # this text shows up as the description in the SCM
+    _svc_description_ = "This service writes stuff to a file"
 
     def __init__(self, args):
-        win32serviceutil.ServiceFramework.__init__(self, args)
+        win32serviceutil.ServiceFramework.__init__(self,args)
+        # create an event to listen for stop requests on
         self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
-        socket.setdefaulttimeout(60)
 
-    def SvcStop(self):
-        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        win32event.SetEvent(self.hWaitStop)
-
+    # core logic of the service
     def SvcDoRun(self):
-        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
-                              servicemanager.PYS_SERVICE_STARTED,
-                              (self._svc_name_, ''))
-        self.main()
+        import servicemanager
+        print 'hello'
 
-    def main(self):
-        while 1:
-            results = win32file.ReadDirectoryChangesW(
-                hDir,
-                1024,
-                True,
-                win32con.FILE_NOTIFY_CHANGE_FILE_NAME |
-                win32con.FILE_NOTIFY_CHANGE_DIR_NAME |
-                win32con.FILE_NOTIFY_CHANGE_ATTRIBUTES |
-                win32con.FILE_NOTIFY_CHANGE_SIZE |
-                win32con.FILE_NOTIFY_CHANGE_LAST_WRITE |
-                win32con.FILE_NOTIFY_CHANGE_SECURITY,
-                None,
-                None
-            )
-            for action, file in results:
-                full_filename = os.path.join(path_to_watch, file)
-                print full_filename, ACTIONS.get(action, "Unknown")
-                # This is where my internal processes get triggered.
-                # Needs a logger at the very least, although a window would be nice too.
-                if action == 1:
-                    if os.path.isfile(full_filename):
-                        # logger.info('New file detected. %s' % full_filename)
-                        copying = True
-                        size2 = -1
-                        while copying:
-                            size = os.stat(full_filename).st_size
-                            if size == size2:
-                                time.sleep(2)
-                                # process_file(full_filename)
-                                q.put(full_filename)
-                                copying = False
-                            else:
-                                size2 = os.stat(full_filename).st_size
-                                time.sleep(2)
+        shit = '//zenbook/Users/sleep/OneDrive/Documents/Scripts/Python/Shotgun/remote_auto_publish/logs/remoteAutoPublish.log'
+        f = open('//zenbook/Users/sleep/OneDrive/Documents/Scripts/Python/Shotgun/remote_auto_publish/logs/test.txt', 'w+')
+        rc = None
+        z = open(shit, 'r')
+        r = ''
+        for d in z:
+            r += str(d)
+        # if the stop event hasn't been fired keep looping
+        while rc != win32event.WAIT_OBJECT_0:
+
+                f.write(r)
+                f.flush()
+                # block for 5 seconds and listen for a stop event
+                rc = win32event.WaitForSingleObject(self.hWaitStop, 5000)
+
+        f.write('SHUTTING DOWN\n')
+        f.close()
+
+    # called when we're being shut down
+    def SvcStop(self):
+        # tell the SCM we're shutting down
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        # fire the stop event
+        win32event.SetEvent(self.hWaitStop)
 
 
 if __name__ == '__main__':
-    win32serviceutil.HandleCommandLine(remoteAutoPublisher)
-
+    win32serviceutil.HandleCommandLine(PySvc)
