8fe0fdce37359e86547e3c15c7f1e275ec360dfb
Adam Benson <AdamBenson.vfx@gmail.com>
2019-03-07 16:52:30 -0800

    Prepping for actual remote test.

--- remote_auto_publish.py
+++ remote_auto_publish.py
@@ -613,97 +613,97 @@ def get_active_shotgun_projects():
 # Watch Folder
 # ---------------------------------------------------------------------------------------------------------------------
 
-# class remoteAutoPublisher(win32serviceutil.ServiceFramework):
-#     _svc_name_ = "RemoteAutoPublisher"
-#     _svc_display_name_ = "Remote Auto Publisher"
-#
-#     def __init__(self, args):
-#         win32serviceutil.ServiceFramework.__init__(self, args)
-#         self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
-#         socket.setdefaulttimeout(60)
-#
-#     def SvcStop(self):
-#         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-#         win32event.SetEvent(self.hWaitStop)
-#
-#     def SvcDoRun(self):
-#         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
-#                               servicemanager.PYS_SERVICE_STARTED,
-#                               (self._svc_name_, ''))
-#         self.main()
-#
-#     def main(self):
-#         while 1:
-#             results = win32file.ReadDirectoryChangesW(
-#                 hDir,
-#                 1024,
-#                 True,
-#                 win32con.FILE_NOTIFY_CHANGE_FILE_NAME |
-#                 win32con.FILE_NOTIFY_CHANGE_DIR_NAME |
-#                 win32con.FILE_NOTIFY_CHANGE_ATTRIBUTES |
-#                 win32con.FILE_NOTIFY_CHANGE_SIZE |
-#                 win32con.FILE_NOTIFY_CHANGE_LAST_WRITE |
-#                 win32con.FILE_NOTIFY_CHANGE_SECURITY,
-#                 None,
-#                 None
-#             )
-#             for action, file in results:
-#                 full_filename = os.path.join(path_to_watch, file)
-#                 print full_filename, ACTIONS.get(action, "Unknown")
-#                 # This is where my internal processes get triggered.
-#                 # Needs a logger at the very least, although a window would be nice too.
-#                 if action == 1:
-#                     if os.path.isfile(full_filename):
-#                         logger.info('New file detected. %s' % full_filename)
-#                         copying = True
-#                         size2 = -1
-#                         while copying:
-#                             size = os.stat(full_filename).st_size
-#                             if size == size2:
-#                                 time.sleep(2)
-#                                 process_file(full_filename)
-#                                 copying = False
-#                             else:
-#                                 size2 = os.stat(full_filename).st_size
-#                                 time.sleep(2)
-#
-#
-# if __name__ == '__main__':
-#     win32serviceutil.HandleCommandLine(remoteAutoPublisher)
+class remoteAutoPublisher(win32serviceutil.ServiceFramework):
+    _svc_name_ = "RemoteAutoPublisher"
+    _svc_display_name_ = "Remote Auto Publisher"
+
+    def __init__(self, args):
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        socket.setdefaulttimeout(60)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_, ''))
+        self.main()
+
+    def main(self):
+        while 1:
+            results = win32file.ReadDirectoryChangesW(
+                hDir,
+                1024,
+                True,
+                win32con.FILE_NOTIFY_CHANGE_FILE_NAME |
+                win32con.FILE_NOTIFY_CHANGE_DIR_NAME |
+                win32con.FILE_NOTIFY_CHANGE_ATTRIBUTES |
+                win32con.FILE_NOTIFY_CHANGE_SIZE |
+                win32con.FILE_NOTIFY_CHANGE_LAST_WRITE |
+                win32con.FILE_NOTIFY_CHANGE_SECURITY,
+                None,
+                None
+            )
+            for action, file in results:
+                full_filename = os.path.join(path_to_watch, file)
+                print full_filename, ACTIONS.get(action, "Unknown")
+                # This is where my internal processes get triggered.
+                # Needs a logger at the very least, although a window would be nice too.
+                if action == 1:
+                    if os.path.isfile(full_filename):
+                        logger.info('New file detected. %s' % full_filename)
+                        copying = True
+                        size2 = -1
+                        while copying:
+                            size = os.stat(full_filename).st_size
+                            if size == size2:
+                                time.sleep(2)
+                                process_file(full_filename)
+                                copying = False
+                            else:
+                                size2 = os.stat(full_filename).st_size
+                                time.sleep(2)
+
+
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(remoteAutoPublisher)
 
 # -------------------------------------------------------------------------------------------------------------
 # TESTING SETUP
 # -------------------------------------------------------------------------------------------------------------
-while 1:
-    results = win32file.ReadDirectoryChangesW(
-        hDir,
-        1024,
-        True,
-        win32con.FILE_NOTIFY_CHANGE_FILE_NAME |
-        win32con.FILE_NOTIFY_CHANGE_DIR_NAME |
-        win32con.FILE_NOTIFY_CHANGE_ATTRIBUTES |
-        win32con.FILE_NOTIFY_CHANGE_SIZE |
-        win32con.FILE_NOTIFY_CHANGE_LAST_WRITE |
-        win32con.FILE_NOTIFY_CHANGE_SECURITY,
-        None,
-        None
-    )
-    for action, file in results:
-        full_filename = os.path.join(path_to_watch, file)
-        print full_filename, ACTIONS.get(action, "Unknown")
-        # This is where my internal processes get triggered.
-        # Needs a logger at the very least, although a window would be nice too.
-        if action == 1:
-            if os.path.isfile(full_filename):
-                logger.info('New file detected. %s' % full_filename)
-                copying = True
-                size2 = -1
-                while copying:
-                    size = os.stat(full_filename).st_size
-                    if size == size2:
-                        time.sleep(2)
-                        process_file(full_filename)
-                        copying = False
-                    else:
-                        size2 = os.stat(full_filename).st_size
-                        time.sleep(2)
+# while 1:
+#     results = win32file.ReadDirectoryChangesW(
+#         hDir,
+#         1024,
+#         True,
+#         win32con.FILE_NOTIFY_CHANGE_FILE_NAME |
+#         win32con.FILE_NOTIFY_CHANGE_DIR_NAME |
+#         win32con.FILE_NOTIFY_CHANGE_ATTRIBUTES |
+#         win32con.FILE_NOTIFY_CHANGE_SIZE |
+#         win32con.FILE_NOTIFY_CHANGE_LAST_WRITE |
+#         win32con.FILE_NOTIFY_CHANGE_SECURITY,
+#         None,
+#         None
+#     )
+#     for action, file in results:
+#         full_filename = os.path.join(path_to_watch, file)
+#         print full_filename, ACTIONS.get(action, "Unknown")
+#         # This is where my internal processes get triggered.
+#         # Needs a logger at the very least, although a window would be nice too.
+#         if action == 1:
+#             if os.path.isfile(full_filename):
+#                 logger.info('New file detected. %s' % full_filename)
+#                 copying = True
+#                 size2 = -1
+#                 while copying:
+#                     size = os.stat(full_filename).st_size
+#                     if size == size2:
+#                         time.sleep(2)
+#                         process_file(full_filename)
+#                         copying = False
+#                     else:
+#                         size2 = os.stat(full_filename).st_size
+#                         time.sleep(2)
