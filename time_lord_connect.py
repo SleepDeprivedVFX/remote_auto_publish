f3990fb269ba850d392ef5b67d031b3bb8d2ff88
Adam Benson <AdamBenson.vfx@gmail.com>
2019-10-08 16:25:04 -0700

    Included logic for testing non-same-day timesheets.  They will now clock themselves out at end of day for whichever day they were clocked in for.

--- time_lord_connect.py
+++ time_lord_connect.py
@@ -7,8 +7,10 @@ The intent is that the IAP can send signals to this utility and update and creat
 from datetime import datetime
 from datetime import timedelta
 from dateutil import parser
-from dateutil import relativedelta
+
 import shotgun_api3 as sgapi
+import logging
+
 # Importing the Time Lord engine libraries
 from bin import configuration
 from bin import shotgun_collect
@@ -19,10 +21,10 @@ from bin import companions
 class time_lord(object):
     def __init__(self, user=None, context=None):
         # Get the configuration file
-        config = configuration.get_configuration()
+        self.config = configuration.get_configuration()
 
         # Connect to Shotgun
-        self.sg = sgapi.Shotgun(config['sg_url'], config['sg_name'], config['sg_key'])
+        self.sg = sgapi.Shotgun(self.config['sg_url'], self.config['sg_name'], self.config['sg_key'])
 
         # Setup User information and Time Continuum
         self.people = companions.companions(self.sg)
@@ -30,9 +32,12 @@ class time_lord(object):
 
         # Set the User from the drag-n-drop
         self.user = self.people.get_user_from_username(username=user)
+        self.last_timesheet = None
 
+        self.run_timesheet(context=context)
+
+    def run_timesheet(self, context=None):
         # Collect the last timesheet
-        # TODO: Need to put in the Ain't today stuff for double checking the stuff especially from Macs
         self.last_timesheet = self.tl_time.get_last_timesheet(user=self.user)
 
         # Start testing the Timesheets.
@@ -43,28 +48,58 @@ class time_lord(object):
             # Get the last start time.
             last_start_dt = self.last_timesheet['sg_task_start']
             last_start = parser.parse('%s %s' % (last_start_dt.date(), last_start_dt.time()))
+
+            # Test to make sure that the last timesheet was clocked out on the correct day.
+            ts_from_today = self.tl_time.aint_today(date=last_start_dt.date())
+            wrong_date_datetime_end = None
+            wrong_date_datetime_start = None
+            if ts_from_today:
+                print 'Last timesheet ain\'t from today: %s' % ts_from_today
+                eod = self.config['regular_end']
+                end_of_day = parser.parse(eod).time()
+                print 'endofday: %s' % end_of_day
+                sod = self.config['regular_start']
+                if last_start_dt.time() < end_of_day:
+                    end_time = end_of_day
+                else:
+                    end_time = last_start_dt.time() + timedelta(minute=1)
+                end_date = last_start_dt.date()
+                wrong_date_datetime_end = parser.parse('%s %s' % (end_date, end_time))
+                wrong_date_datetime_start = parser.parse('%s %s' % (datetime.now().date(), sod))
+
             # Create a short delayed entry from the last start
-            pseudo_end = last_start + timedelta(seconds=30)
+            pseudo_end = last_start + timedelta(seconds=1)
             last_task_id = str(self.last_timesheet['entity']['id'])
             current_task_id = str(context['Task']['id'])
             if current_task_id != last_task_id:
                 print 'Retroactive clock-out of previous timesheet...'
-                self.tl_time.clock_out_time_sheet(timesheet=self.last_timesheet, clock_out=pseudo_end)
-                print 'Creating new retroactive timesheet block...'
-                ts_block = self.tl_time.create_new_timesheet(user=self.user, context=context, start_time=pseudo_end,
-                                                             entry='Drag-n-Drop')
+                if wrong_date_datetime_end:
+                    self.tl_time.clock_out_time_sheet(timesheet=self.last_timesheet, clock_out=wrong_date_datetime_end)
+                    print 'Creating new retroactive timesheet block...'
+                    ts_block = self.tl_time.create_new_timesheet(user=self.user, context=context,
+                                                                 start_time=wrong_date_datetime_start,
+                                                                 entry='Drag-n-Drop')
+                else:
+                    self.tl_time.clock_out_time_sheet(timesheet=self.last_timesheet, clock_out=pseudo_end)
+                    print 'Creating new retroactive timesheet block...'
+                    ts_block = self.tl_time.create_new_timesheet(user=self.user, context=context, start_time=pseudo_end,
+                                                                 entry='Drag-n-Drop')
                 self.tl_time.clock_out_time_sheet(timesheet=ts_block, clock_out=datetime.now())
                 print 'Creating New Current timesheet...'
                 self.tl_time.create_new_timesheet(user=self.user, context=context, start_time=datetime.now(),
                                                   entry='Drag-n-Drop')
-                print 'New Timesheet created for %s' % user
+                print 'New Timesheet created for %s' % self.user['name']
             elif current_task_id == last_task_id:
-                print 'Creating retroactive timsheet block...'
-                self.tl_time.clock_out_time_sheet(timesheet=self.last_timesheet, clock_out=datetime.now())
+                if wrong_date_datetime_end:
+                    print 'Creating retroactive timsheet block...'
+                    self.tl_time.clock_out_time_sheet(timesheet=self.last_timesheet, clock_out=wrong_date_datetime_end)
+                else:
+                    print 'Creating retroactive timsheet block...'
+                    self.tl_time.clock_out_time_sheet(timesheet=self.last_timesheet, clock_out=datetime.now())
                 print 'Creating new timesheet...'
                 self.tl_time.create_new_timesheet(user=self.user, context=context, start_time=datetime.now(),
                                                   entry='Drag-n-Drop')
-                print 'New Timesheet Created for %s' % user
+                print 'New Timesheet Created for %s' % self.user['name']
 
 
 
