795ba949ef218a18ee1f0ed20be66bf331ec8019
Adam Benson <AdamBenson.vfx@gmail.com>
2020-03-02 19:45:07 -0800

    Progressively upgrading the Remote Publisher.  Need to test against the original

--- IAP_Startup.bat
+++ IAP_Startup.bat
@@ -1,2 +1,2 @@
 TITLE ASC Internal Auto Publishing Utility
-"c:/python27_32/python.exe" "c:/shotgun/remote_auto_publish/internal_auto_publish.py"
+"c:/python37/python.exe" "c:/shotgun/remote_auto_publish/internal_auto_publish.py"
